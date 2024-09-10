4aebc1af8834b7397aaf2ca745902c32d3fc7437
pjunstall <pjtunstall@gmail.com>
2024-09-10 17:25:11 +0100

    rightward carousel working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+import fool from "./images/0_fool.jpg";
+import emperor from "./images/4_emperor.jpg";
+import justice from "./images/8_justice.jpg";
+import chariot from "./images/7_chariot.jpg";
+import fortune from "./images/10_fortune.jpg";
+
+let cards = [justice, chariot, fool, emperor, fortune];
+
+function Card({ src, x }) {
+  let size;
+  switch (x) {
+    case 1:
+      size = "small-image";
+      break;
+    case 2:
+      size = "large-image";
+      break;
+    case 3:
+      size = "small-image";
+      break;
+    default:
+      size = "hidden-image";
+  }
+  return <img src={src} className={`image ${size}`} alt="" />;
+}
 
 function App() {
+  const [moveX, setMoveX] = useState(false);
+
+  const handleClick = (event) => {
+    if (event.clientX < window.innerWidth / 2) {
+      cards.unshift(cards.pop());
+      setMoveX(!moveX);
+    } else {
+      cards.push(cards.shift());
+      setMoveX(!moveX);
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="App" onClick={handleClick}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {cards.map((src, i) => (
+          <Card src={src} x={i} key={src} />
+        ))}
       </header>
     </div>
   );
