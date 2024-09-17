f0020ba1749a1a61662671650f18a6f646cbf863
pjunstall <pjtunstall@gmail.com>
2024-09-17 22:11:02 +0100

    extracted handleClickOrDoubleClick

--- src/App.js
+++ src/App.js
@@ -57,57 +57,6 @@ function App() {
   const owlAudioRef = useRef(new Audio(owlSound));
   const shuffleAudioRef = useRef(new Audio(shuffleSound));
 
-  const handleClickOrDoubleClick = useCallback(
-    (event) => {
-      event.preventDefault();
-      if (isMoving) return;
-
-      clickCountRef.current += 1;
-
-      if (clickCountRef.current === 1) {
-        clickTimeoutRef.current = setTimeout(() => {
-          if (clickCountRef.current === 1) {
-            // Single click action
-            if (event.clientX < window.innerWidth / 2) {
-              timeoutRef.current = moveCards(
-                1,
-                setCards,
-                setIsMoving,
-                transitionDuration
-              );
-            } else {
-              timeoutRef.current = moveCards(
-                -1,
-                setCards,
-                setIsMoving,
-                transitionDuration
-              );
-            }
-          }
-          clickCountRef.current = 0;
-        }, transitionDuration);
-      } else if (clickCountRef.current === 2) {
-        clearTimeout(clickTimeoutRef.current);
-        // Double click action
-        const audioClone = flipAudioRef.current.cloneNode();
-        audioClone.play();
-
-        setTimeout(() => {
-          flipAllCards(setCards);
-
-          setTimeout(() => {
-            setCards((prevCards) =>
-              prevCards.map((card) => ({ ...card, isAnimating: false }))
-            );
-          }, transitionDuration);
-        }, 20);
-
-        clickCountRef.current = 0;
-      }
-    },
-    [isMoving]
-  );
-
   const handleKeyDown = useCallback(
     (event) => {
       event.preventDefault();
@@ -215,7 +164,19 @@ function App() {
     <div
       className="App"
       ref={appRef}
-      onClick={handleClickOrDoubleClick}
+      onClick={(event) => {
+        handleClickOrDoubleClick(
+          event,
+          isMoving,
+          clickCountRef,
+          clickTimeoutRef,
+          timeoutRef,
+          transitionDuration,
+          setCards,
+          setIsMoving,
+          flipAudioRef
+        );
+      }}
       tabIndex="0"
       onKeyDown={handleKeyDown}
       onKeyUp={handleKeyUp}
@@ -252,6 +213,64 @@ function App() {
       )}
     </div>
   );
+}
+
+function handleClickOrDoubleClick(
+  event,
+  isMoving,
+  clickCountRef,
+  clickTimeoutRef,
+  timeoutRef,
+  transitionDuration,
+  setCards,
+  setIsMoving,
+  flipAudioRef
+) {
+  event.preventDefault();
+  if (isMoving) return;
+
+  clickCountRef.current += 1;
+
+  if (clickCountRef.current === 1) {
+    clickTimeoutRef.current = setTimeout(() => {
+      if (clickCountRef.current === 1) {
+        // Single click action
+        if (event.clientX < window.innerWidth / 2) {
+          timeoutRef.current = moveCards(
+            1,
+            setCards,
+            setIsMoving,
+            transitionDuration
+          );
+        } else {
+          timeoutRef.current = moveCards(
+            -1,
+            setCards,
+            setIsMoving,
+            transitionDuration
+          );
+        }
+      }
+      clickCountRef.current = 0;
+    }, transitionDuration);
+  } else if (clickCountRef.current === 2) {
+    clearTimeout(clickTimeoutRef.current);
+    // Double click action
+    const audioClone = flipAudioRef.current.cloneNode();
+    audioClone.play();
+
+    setTimeout(() => {
+      flipAllCards(setCards);
+
+      setTimeout(() => {
+        setCards((prevCards) =>
+          prevCards.map((card) => ({ ...card, isAnimating: false }))
+        );
+      }, transitionDuration);
+    }, 20);
+
+    clickCountRef.current = 0;
+  }
 }
 
 function handleCardClick(
